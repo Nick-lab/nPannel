2f2c709e51f9be282f86838c65c132f4f217c758
root <root@li1073-81.members.linode.com>
2020-05-05 14:37:36 +0000

    tracked 1st client

--- clients/1/page_scripts/16ab6cc794a.js
+++ clients/1/page_scripts/16ab6cc794a.js
@@ -1,5 +1,2 @@
 module.exports = (options) => {
-    if(options.session.views) options.session.views += 1;
-    else options.session.views = 1;
-    return {session: options.session, get: options.GET};
 }
